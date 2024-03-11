5b4e1aebda3b30841ec621e7042c81179e70bec3
kattela6 <vaishnavikattela999@gmail.com>
2024-03-11 22:13:18 +0530

    Data Transformstion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -22,7 +24,7 @@ class DataIngestion:
             logging.info('Exported or read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            #df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             logging.info("Train test split initiated")
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             train_set,test_set= train_test_split(df,test_size=0.2,random_state=42)
@@ -39,4 +41,7 @@ class DataIngestion:
             raise CustomException(e,sys)
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.inititate_data_ingestion()
+    train_data,test_data=obj.inititate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
